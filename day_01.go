846055933994d92f7f641826ac59df0d951c9f38
Tyler Brittain <tbrittain100@gmail.com>
2025-12-10 21:18:52 -0800

    d1p2

--- day_01.go
+++ day_01.go
@@ -9,6 +9,7 @@ import (
 
 func main() {
 	fmt.Println(p1("01_pt1.txt"))
+	fmt.Println(p2("01_pt1.txt"))
 }
 
 func p1(fileName string) (int, error) {
@@ -33,14 +34,10 @@ func p1(fileName string) (int, error) {
 		} else {
 			return 0, fmt.Errorf("unexpected starting char")
 		}
-		fmt.Printf("prenorm: %d\n", pos)
 		pos = normalizePos(pos)
-		fmt.Printf("post: %d\n", pos)
 		if pos == 0 {
 			count += 1
 		}
-
-		fmt.Println(pos)
 
 	}
 
@@ -50,3 +47,54 @@ func p1(fileName string) (int, error) {
 func normalizePos(num int) int {
 	return ((num % 100) + 100) % 100
 }
+
+func p2(fileName string) (int, error) {
+	f, err := os.Open(fileName)
+	if err != nil {
+		return 0, fmt.Errorf("unable to open file: %v", err)
+	}
+
+	scanner := bufio.NewScanner(f)
+
+	count := 0
+	pos := 50
+	for scanner.Scan() {
+		num, err := strconv.Atoi(scanner.Text()[1:])
+		if err != nil {
+			return 0, fmt.Errorf("unable to parse int: %v", err)
+		}
+		dir := scanner.Text()[0]
+
+		fmt.Println(num)
+		for num > 0 {
+			pos = tick(pos, dir)
+			if pos == 100 {
+				pos = 0
+			}
+			if pos == 0 {
+				count++
+			}
+			if pos == -1 {
+				pos = 99
+			}
+			num--
+		}
+
+	}
+
+	return count, nil
+}
+
+func tick(start int, dir byte) int {
+	if dir == 'L' {
+		start--
+	} else {
+		start += 1
+	}
+	return start
+
+}
+
+// func normalizePos2(num int) (int, bool) {
+// return ((num % 100) + 100) % 100, false
+// }
--- day_01_test.go
+++ day_01_test.go
@@ -29,3 +29,29 @@ func TestPt1(t *testing.T) {
 		)
 	}
 }
+
+func TestPt2(t *testing.T) {
+	cases := []struct {
+		caseName    string
+		fileName    string
+		expectedAns int
+	}{
+		{
+			caseName:    "sample",
+			fileName:    "01_sample.txt",
+			expectedAns: 6,
+		},
+	}
+	for _, tt := range cases {
+		t.Run(tt.caseName, func(t *testing.T) {
+			output, err := p2(tt.fileName)
+			if err != nil {
+				t.Errorf("Encountered error: %v", err)
+			}
+			if output != tt.expectedAns {
+				t.Errorf("Expected %v, got %v", tt.expectedAns, output)
+			}
+		},
+		)
+	}
+}
